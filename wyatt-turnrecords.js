051150da1ebaff7cbce22f981d66b1ba40de4ba3
wyattlaberge64 <labergew@bsdvt.org>
2019-05-09 11:01:29 -0400

    updated

---
+++ wyatt-turnrecords.js
@@ -0,0 +1,12 @@
+// Create TurnRecords Table: An array of 9 arrays. 
+// Each of the 9 arrays has 9 elements.
+// Each of the elements contains three items: Row (0-8), Column (0-8), Color (B, W, E for empty)
+// Document from https://www.youtube.com/watch?v=D5-zdG-r_yQ
+var turnRecords = [[1,1,"w"],[1,2,"b"],[1,4,"b"]
+
+// Build Board
+// For Loop Columns: 0-8
+// For Loop Rows: 0-8
+// Build Rows as you go of LIs. 
+// For each Row item, create an LI. 
+// Read the TurnRecords for that LI and append the class to it. 

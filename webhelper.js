144de7e5adc70e6908abccd3b19fba04be9128ce
wyattlaberge64 <42870908+wyattlaberge64@users.noreply.github.com>
2019-12-18 11:38:13 -0500

    Update webhelper.js

--- webhelper.js
+++ webhelper.js
@@ -72,7 +72,7 @@ function removeLastTurn(){
 	}
 }
 function teachingGame(turns){
-	turns = [[1,1,"b",2,[]],[1,2,"w",1,[]],[2,2,"b",0,[]],[1,0,"w",0,[]],[1,3,"b",1,[]],[2,1,"w",1,[]],[0,2,"b",1,[1,2]],[0,1,"w",1,[]],[0,0,"b",1,[0,1]],[3,0,"w",1,[]],[2,0,"b",1,[1,0]],[3,1,"w",1,[]],[1,0,"b",1,[]]];
+	turns = [[1,1,"b",2,[]],[1,2,"w",2,[]],[2,2,"b",0,[]],[1,0,"w",0,[]],[1,3,"b",1,[]],[2,1,"w",1,[]],[0,2,"b",1,[1,2]],[0,1,"w",1,[]],[0,0,"b",1,[0,1]],[3,0,"w",1,[]],[2,0,"b",1,[1,0]],[3,1,"w",1,[]],[1,0,"b",1,[]]];
 	gameMessages=[[13,"Now, you could. So I'm blocking that."],[12,"And I can't take you back because that would be Ko."],[11,"Yes, I suppose so. Atari!"],[10," Shouldn\t you have said Atari again?"],[9," That would be Ko, but since I can remove the threat, I will."],[8,"Atari"],[7,"Gotcha."],[6,"Atari to you!"],[5,"Atari!"],[1,"Hello"],[0,"Hi"]];
 	return turns;
 }
@@ -258,7 +258,17 @@ function showMessage(turn){
 		// see if there is a message for this turn
 		if (gameMessages[messageMatch][0]==turn+1){
 			if (turns[turn][3]==2) {
-				alert("an interaction!");
+				switch(turn) {
+					case 0:
+						alert("an interaction!");
+						break;
+					case 1:
+						hello();
+						break;
+					default:
+						// code block
+				}
+				
 			}
 			else if (turns[turn][3]==1) {
 				message=gameMessages[messageMatch][1];
@@ -271,4 +281,12 @@ function showMessage(turn){
 	}
 	refreshBox(messages,message,"text");
 	return message;
+}
+/* Interactions */
+function hello(){
+	let status = prompt("How are you? ");
+	if (status == "ok"){
+		alert("good");
+	}
+	else alert("sorry to here that.");
 }
